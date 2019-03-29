7ac2bdf336a889d86175883ff2ba871ef65eab32
Vimal Sheoran <vimalpsheoran@gmail.com>
2019-03-29 18:35:09 +0530

    Implemented git helper functions but, implementation for readline() is buggy, gonna fix this tomorrow

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 
 const builder = require("./app_builder/builder");
 const exec = require("child_process").exec;
+const gitHelper = require("./app_builder/gitHelper");
+const readLine = require("readline");
 
 // Command line helpers here
 
@@ -9,6 +11,7 @@ const cmdArg	=	process.argv[1] // storing the calling command name
 const optArg1	=	process.argv[2] // storing first option
 const optArg2	=	process.argv[3] // storing second option
 const paramArg	=	process.argv[4] // storing the parameter argument
+let appDir; // store the location of the directory where the app is initialised
 
 const showHelpForMake = () => {
 	console.log("Usage: ");
@@ -31,14 +34,39 @@ if (optArg1 == "make"){
 	switch (optArg2){
 
 		case "app":
-		const dirName = paramArg;
 		console.log("Setting up scaffolds...");
 		builder
-			.initAppStructure(dirName)
-			.then((appDir) => {
-				console.log(appDir);
-				console.log("Done!");
-			});
+			.initAppStructure(paramArg)
+			.then(async(appDir) => {
+				console.log(
+					`Generated application structure at ${appDir}`);
+				return appDir;})
+			.then(async(appDir) => {
+				console.log("Do you want to initialize a git repo? y/n");
+				let choice = readLine().toLowerCase();
+				if (choice == "y" ||
+					choice == "yes"){
+					gitHelper
+						.initGitRepo(appDir)
+						.then(() => {
+							console.log("Do you want to add a remote repository? y/n");
+							choice = readLine().toLowerCase();
+							if (choice == "y" ||
+								choice == "yes"){
+								console.log("Add your remote url: ");
+								let url = readLine();
+								gitHelper
+									.addRemote(
+										appDir,
+										url);
+								return;
+							}
+						})
+				}
+				return appDir;
+			})
+
+
 		break;
 
 		case "api":
@@ -53,5 +81,3 @@ if (optArg1 == "make"){
 } else {
 	showHelpForMake();
 }
-
-
