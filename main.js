00611e849c8d515bdeeb97a7a7c0af0041394b9c
Vimal Sheoran <vimalpsheoran@gmail.com>
2019-03-31 11:22:20 +0530

    Code refactoring and added support for code generation for components

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const builder = require("./lib/builder");
+const componentHelper = require("./lib/componentHelper");
 const exec = require("await-exec");
 const fs = require("fs");
 const gitHelper = require("./lib/gitHelper");
@@ -38,93 +39,96 @@ const run = async (optArg1, optArg2) => {
 		switch (optArg2){
 
 			case "app":
-			if (!paramArg ||
-				paramArg == ""){
-				showHelpForMake();
-				break;
-			}
-			console.log("Setting up scaffolds...");
-			let appDir = await builder
-				.initAppStructure(paramArg);
-			console.log(`Generated application at ${appDir}`);
+				
+				if (!paramArg ||
+					paramArg == ""){
+					showHelpForMake();
+					break;
+				}
+				console.log("Setting up scaffolds...");
+				let appDir = await builder
+					.initAppStructure(paramArg);
+				console.log(`Generated application at ${appDir}`);
 
-			let initGit = await menu.basicGitSetupPrompt();
+				let initGit = await menu.basicGitSetupPrompt();
 
-			if (initGit.choice.toLowerCase() == "y" ||
-				initGit.choice.toLowerCase() == "yes"){
-				await gitHelper
-					.initGitRepo(appDir);
-				console.log("Successfully initialised a git repository!");
-
-				let setGitRemote = await menu.gitRemoteSetupPrompt();
-
-				if (setGitRemote.choice.toLowerCase() == "y" ||
-					setGitRemote.choice.toLowerCase() == "yes"){
-					let remoteUrl = await menu.getGitRemoteUrl();
+				if (initGit.choice.toLowerCase() == "y" ||
+					initGit.choice.toLowerCase() == "yes"){
 					await gitHelper
-						.addRemote(appDir, remoteUrl.url);				
+						.initGitRepo(appDir);
+					console.log("Successfully initialised a git repository!");
+
+					let setGitRemote = await menu.gitRemoteSetupPrompt();
+
+					if (setGitRemote.choice.toLowerCase() == "y" ||
+						setGitRemote.choice.toLowerCase() == "yes"){
+						let remoteUrl = await menu.getGitRemoteUrl();
+						await gitHelper
+							.addRemote(appDir, remoteUrl.url);				
+					} else {
+						console.log("Skipping...");
+					}
 				} else {
 					console.log("Skipping...");
 				}
-			} else {
-				console.log("Skipping...");
-			}
 
-			console.log("Entering package setup...");
-			let packageOpts = await menu.packageInitOptions();
-			if (packageOpts["keywords"] == ""){
-				packageOpts["keywords"] = [];
-			} else {
-				let sanatizedKeywords = packageOpts["keywords"]
-					.split(" ");
-				packageOpts["keywords"] = sanatizedKeywords;
+				console.log("Entering package setup...");
+				let packageOpts = await menu.packageInitOptions();
+				if (packageOpts["keywords"] == ""){
+					packageOpts["keywords"] = [];
+				} else {
+					let sanatizedKeywords = packageOpts["keywords"]
+						.split(" ");
+					packageOpts["keywords"] = sanatizedKeywords;
 
-			}
+				}
 
-			await packageHelper
-				.initPackage(paramArg, appDir, packageOpts);
+				await packageHelper
+					.initPackage(paramArg, appDir, packageOpts);
 
-			console.log("Created package.json!");
-			await packageHelper
-				.installVonagDependencies(appDir);
+				console.log("Created package.json!");
+				await packageHelper
+					.installVonagDependencies(appDir);
 
-			let userDeps = await menu.userDependenciesList();
+				let userDeps = await menu.userDependenciesList();
 
-			let val = await packageHelper
-				.installUserDependencies(
-					appDir,
-					userDeps.dependencies.split(" "));
-			break;
+				let val = await packageHelper
+					.installUserDependencies(
+						appDir,
+						userDeps.dependencies.split(" "));
+				break;
 
 			case "component":
 			
-			if (!paramArg ||
-				paramArg == ""){
-				showHelpForMake();
-				break;
-			}
+				if (!paramArg ||
+					paramArg == ""){
+					showHelpForMake();
+					break;
+				}
 
-			try {
-				
-				let unparsedMeta = fs
-					.readFileSync("package.json");
-				let appMeta = JSON.parse(unparsedMeta);
+				try {
+					
+					let unparsedMeta = fs
+						.readFileSync("package.json");
+					let appMeta = JSON.parse(unparsedMeta);
 
-				if (!appMeta["vonagApp"] ||
-					!(appMeta["vonagApp"] == "true"))
-					throw new Error();
+					if (!appMeta["vonagApp"] ||
+						!(appMeta["vonagApp"] == "true"))
+						throw new Error();
 
-				let componentDir = process.cwd() + "/components/" + `${paramArg}`;
+					let componentDir = process.cwd() + 
+						"/components/" + 
+						`${paramArg}`;
 
-				await exec(
-					`mkdir ${componentDir} &&
-					cd ${componentDir} &&
-					touch ${paramArg}.controller.js &&
-					touch ${paramArg}.routes.js &&
-					touch ${paramArg}.models.js`);
+					await componentHelper.generateComponent(
+							componentDir,
+							paramArg
+						);
 
 			} catch(err){
-				console.log("Invalid operation, Not inside a vonag application");
+				console.log(err);
+				console.log("Invalid operation, Not inside a vonag application directory");
+				console.log("Go to the main application folder to generate a component");
 				break;
 			}
 
